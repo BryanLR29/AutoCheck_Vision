1a7c4f380ef2876df0a8a7d37c4b19ec4ab49c84
BryanLR29 <303.lopez.ruiz.bryan@gmail.com>
2026-06-15 00:33:46 -0600

    chore: dejar main como rama base limpia

--- ocr_test.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['en'])
-
-resultado = reader.readtext("placa_detectada.jpg")
-
-for r in resultado:
-    print("Texto:", r[1])
-    print("Confianza:", round(r[2] * 100, 2), "%")
